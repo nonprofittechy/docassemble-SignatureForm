b9a15521a31569175d8a0663122b65cdb35c0b6f
System Administrator <qsteenhuis@gmail.com>
2021-10-16 09:34:29 -0400

    Add branding

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.SignatureForm',
-      version='0.0.1',
+      version='0.1.0',
       description=('A docassemble extension.'),
       long_description='# docassemble.SignatureForm\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@lemmalegal.com\r\n\r\n',
       long_description_content_type='text/markdown',
